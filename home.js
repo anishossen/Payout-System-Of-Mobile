54c26b7dc4a73e0024f3f0128f838a77eb684b2b
anishossen <mdanishossen71@gmail.com>
2024-09-21 15:09:54 +0600

    addes second code

--- home.js
+++ home.js
@@ -4,6 +4,20 @@ document.getElementById('add-money-btn').addEventListener('click', function(even
 
     const inputMoney = document.getElementById('input-money').value ;
     const inputPin = document.getElementById('input-pin').value ;
-    
+    // step : 2
+    if(inputPin === '12345'){
+        // step : 1
+        const balance = document.getElementById('main-balance').innerText;
+        // step : 2
+        const convertMoney = parseInt(inputMoney);
+        const convertBalance = parseInt(balance);
+
+        const addedBalance = convertMoney + convertBalance;
+        // step : 3
+        document.getElementById('main-balance').innerText = addedBalance;
+    }
+    else{
+        alert('Please type valid number')
+    }
 
 })
